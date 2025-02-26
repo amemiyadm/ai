ba6b7998644f2c5b3880d397310103525aceb316
amemiyadm <108377846+amemiyadm@users.noreply.github.com>
2025-02-26 22:46:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-#!/usr/local/bin/python3.7
-
-import pickle
+from flask import Flask, jsonify, request
 import torch
-from flask import Flask, render_template, request, jsonify
-from sentence_transformers import util
 from transformers import BertJapaneseTokenizer, BertModel
-import google.generativeai as genai
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 class SentenceBertJapanese:
+
     def __init__(self, model_name_or_path, device=None):
-        self.tokenizer = BertJapaneseTokenizer.from_pretrained(model_name_or_path)
+        self.tokenizer = BertJapaneseTokenizer.from_pretrained(
+            model_name_or_path)
         self.model = BertModel.from_pretrained(model_name_or_path)
         self.model.eval()
 
@@ -23,8 +22,10 @@ class SentenceBertJapanese:
 
     def _mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output[0]
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(
+            token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded,
+                         1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def encode(self, sentences, batch_size=8):
         all_embeddings = []
@@ -32,65 +33,28 @@ class SentenceBertJapanese:
         for batch_idx in iterator:
             batch = sentences[batch_idx:batch_idx + batch_size]
 
-            encoded_input = self.tokenizer.batch_encode_plus(batch, padding="longest",
-                                                             truncation=True, return_tensors="pt").to(self.device)
+            encoded_input = self.tokenizer.batch_encode_plus(
+                batch, padding="longest", truncation=True,
+                return_tensors="pt").to(self.device)
             model_output = self.model(**encoded_input)
-            sentence_embeddings = self._mean_pooling(model_output, encoded_input["attention_mask"]).to('cpu')
+            sentence_embeddings = self._mean_pooling(
+                model_output, encoded_input["attention_mask"]).to('cpu')
 
             all_embeddings.extend(sentence_embeddings)
 
-        # return torch.stack(all_embeddings).numpy()
         return torch.stack(all_embeddings)
 
 
 model = SentenceBertJapanese('sonoisa/sentence-bert-base-ja-mean-tokens-v2')
 
-genai.configure(api_key='AIzaSyAkzQkK_NZsFXK-jZoF9j51zV7F7KWNdm0')
-google_model = genai.GenerativeModel("gemini-2.0-flash")
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/search', methods=['POST'])
-def search():
-    with open("embeddings.pkl", "rb") as f:
-        loaded_embeddings = pickle.load(f)
-
+@app.route("/process", methods=["POST"])
+def hello():
+    print(request.form['user-input'])
     user_input = request.form['user-input']
-
-    embeddings2 = model.encode([user_input], batch_size=8)
-
-    most_similar = -1
-    most_idx = -1
-
-    # コサイン類似度の計算
-    for i, loaded_embedding in enumerate(loaded_embeddings):
-        cosine_scores = util.cos_sim(loaded_embedding['vector'], embeddings2)
-        best_idx = torch.argmax(cosine_scores)
-        best_score = cosine_scores[best_idx]
-        if most_similar < best_score:
-            most_similar = best_score
-            most_idx = i
-
-    # 関連性の高い文章を取得
-    most_relevant = loaded_embeddings[most_idx]
-
-    prompt = f'''
-    3つの条件を守り、以下の記事から「{user_input}」について記述してある部分を簡潔に説明してください。
-    条件1.丁寧過ぎない適度な敬語で回答する
-    条件2.回答に説明以外の前置きなどを含まない
-    条件3.「こちらの記事では、」から説明を始める
-    
-    {most_relevant}
-    '''
-
-    response = google_model.generate_content(prompt)
-
-    return jsonify({'output': response.text})
+    embeddings = model.encode([user_input], batch_size=8)
+    return jsonify({'output': embeddings.tolist()})
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
